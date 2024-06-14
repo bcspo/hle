53733ad0b9db0bdb15c3548024421eab20eac824
bcspo <169110098+bcspo@users.noreply.github.com>
2024-06-14 15:45:46 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 $(function () {
-    $('#content').fullpage();
+
+    const h = $('.y_img').height();
+
+    $('#content').fullpage({
+        afterLoad: function (e, idx) {
+            $(".y_img").animate({ top: -(h / 7 * (idx - 1)) }, 1500)
+        }
+    });
 
     $('#header .utils .lang>a').on('click', function (e) {
         e.preventDefault();
